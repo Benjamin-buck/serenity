e015fb9fb4534e15c1a889307a35e4f32f47d6f7
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-11-08 12:20:49 -0500

    Adds FAQ page

--- constants/data.ts
+++ constants/data.ts
@@ -1,6 +1,13 @@
+// Navigation Links
 export const navigationLinks = [
   { label: "Home", href: "/" },
   { label: "Services", href: "/services" },
   { label: "About", href: "/about" },
   { label: "FAQ", href: "/faq" },
 ];
+
+
+// FAQ Questions and Answers:
+export const faq = [
+  {question: 'This is a question', answer: 'This is an answer'},
+]
