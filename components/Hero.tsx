3f6b6820553e7c342f4a9808b3cc03dff7b14a74
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-11-09 14:49:25 -0500

    Adds gsap animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,31 @@
+"use client";
 import React from "react";
 import Button from "./Button";
 import { MdRequestQuote } from "react-icons/md";
 import Image from "next/image";
 import { home } from "@/constants/data";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero",
+      {
+        opacity: 0,
+        y: "100px",
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        delay: 0.5,
+      }
+    );
+  });
   return (
     <div className="w-full h-[82vh]">
-      <div className="container mx-auto max-md:px-6 flex max-lg:flex-col-reverse">
+      <div className="hero container mx-auto max-md:px-6 flex max-lg:flex-col-reverse">
         <div className="w-[50%] max-lg:w-full  h-[78vh] max-lg:h-auto max-lg:py-10 flex items-center justify-center">
           <div className="space-y-3">
             <h2 className="text-6xl max-md:text-4xl font-bold">
